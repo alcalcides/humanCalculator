ec1d213678dcc41eebd339642e8025d5a10a9966
alcalcides <alcalcides@hotmail.com>
2019-05-15 00:14:13 -0300

    At level 5, the operands' heuristic were changed so that there were no responses smaller than zero

--- _src/_levels/nivel5.h
+++ _src/_levels/nivel5.h
@@ -20,6 +20,8 @@ bool jogarNivel5(unsigned int &life, unsigned int &bonus, bool &gameOver, string
 	for(i = 0; i < 30 && !gameOver; i++ ){
 		op1 = rand() % 11;
 		op2 = rand() % 11;
+		if(op1 < op2)
+			swap(op1, op2);
 		cout << op1 << " - " << op2 << " = ";
 		cin >> res;
 		if(acertouN5(op1, op2, res)){
@@ -56,6 +58,5 @@ bool jogarNivel5(unsigned int &life, unsigned int &bonus, bool &gameOver, string
 bool acertouN5(int i, int j, int res){
 	bool acert;
 	acert = (i-j == res)? true : false;
-	if((i - j) < 0 && res == 0) acert = true; //o jogador não precisa saber números negativos
 	return acert;
 }
